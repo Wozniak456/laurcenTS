9d7f801b6b7edb36384adf2641b90c1e9b160910
Petro Vozniak <woznyak@ukr.net>
2025-01-12 17:26:58 +0200

    purchtable now dynamic

--- src/app/purchtable/view/page.tsx
+++ src/app/purchtable/view/page.tsx
@@ -1,56 +1,61 @@
 import { db } from "@/db";
 
-import PurchTableComponent from "@/components/PurchHeaders/purchtable-table"
+import PurchTableComponent from "@/components/PurchHeaders/purchtable-table";
+
+export const dynamic = "force-dynamic";
 
 export default async function PurchtableHome() {
   const purchtables = await db.purchtable.findMany({
-    select:{
+    select: {
       id: true,
       doc_id: true,
       date_time: true,
       vendor_doc_number: true,
       vendor_id: true,
       vendors: {
-        select:{
+        select: {
           id: true,
           name: true,
           description: true,
-          items: true
-        }
+          items: true,
+        },
       },
       purchaselines: {
-        select:{
+        select: {
           id: true,
           item_id: true,
           units: true,
           quantity: true,
           items: {
-            select:{
+            select: {
               id: true,
               name: true,
-              feed_type_id: true
-            }
-          }
-        }
-      }
-    }
+              feed_type_id: true,
+            },
+          },
+        },
+      },
+    },
   });
 
-  const vendors = await db.vendors.findMany()
+  const vendors = await db.vendors.findMany();
 
   const items = await db.items.findMany({
-    select:{
+    select: {
       id: true,
       name: true,
       units: true,
-      vendor_id: true 
-    }
-  })
-
+      vendor_id: true,
+    },
+  });
 
   return (
     <div>
-      <PurchTableComponent purchtables={purchtables} vendors={vendors} items={items}/>
+      <PurchTableComponent
+        purchtables={purchtables}
+        vendors={vendors}
+        items={items}
+      />
     </div>
   );
 }
