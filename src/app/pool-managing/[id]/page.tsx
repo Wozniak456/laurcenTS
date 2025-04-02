76cfddd0fc3525718f5bd1995d481526e36d0931
Petro Vozniak <woznyak@ukr.net>
2025-04-02 03:09:34 +0300

    removed pool-managing\view, fixed ActualizationPage arguments

--- src/app/pool-managing/[id]/page.tsx
+++ src/app/pool-managing/[id]/page.tsx
@@ -54,6 +54,7 @@ export default async function PoolManagingShowPage(
           poolInfo={poolInfo}
           batches={batches}
           feeds={feeds}
+          today={props.params.day}
         />
       </>
     );
